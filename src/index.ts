9974278d80e9c94247c65780b6f9ccedb6f90825
Angel Velasquez <pcisavel@upc.edu.pe>
2024-08-26 22:08:51 -0500

    feat(main): implemented main behavior.

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,15 @@
+import {Customer} from "./crm/domain/model/customer";
+import {SalesOrder} from "./sales/domain/model/sales-order";
+
 console.log('Thank you for using WebStorm 💙')
+
+let customer = new Customer("John Doe");
+
+let salesOrder = new SalesOrder(customer.id);
+
+salesOrder.addItem("P001", 2, 100);
+salesOrder.addItem("P002", 20, 50);
+
+customer.lastOrderPrice = salesOrder.calculateTotalPrice();
+
+console.log(`Customer named ${customer.name}, and identified with ID ${customer.id} have just completed an order for a total of ${customer.lastOrderPrice}`);
